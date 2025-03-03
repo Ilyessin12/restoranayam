c561524a2bd840fb707d77497ae3fe9e6f550a83
Juthersse <ichsankhairullah@upi.edu>
2025-03-03 13:47:23 +0700

    Ganti css ke tailwind, lihat di 178.0.0.1:8000/beranda

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -5,13 +5,135 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
+    <div
+      class="container max-w-lg px-4 py-32 mx-auto text-left bg-center bg-no-repeat bg-cover md:max-w-none md:text-center"
+      style="background-image: url('https://cdn.pixabay.com/photo/2016/11/18/14/39/beans-1834984_960_720.jpg')">
+      <h1
+        class="font-mono text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-orange-400 to-red-500 md:text-center sm:leading-none lg:text-5xl">
+        <span class="inline md:block">Welcome To Larainfo Restaurant</span>
+      </h1>
+      <div class="mx-auto mt-2 text-orange-50 md:text-center lg:text-lg">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta voluptatem ab necessitatibus illo praesentium
+        culpa excepturi quae commodi quaerat,
+      </div>
+      <div class="flex flex-col items-center mt-12 text-center">
+        <span class="relative inline-flex w-full md:w-auto">
+          <a href="#_" type="button"
+            class="inline-flex items-center justify-center px-6 py-2 text-base font-bold leading-6 text-white bg-orange-600 rounded-full lg:w-full md:w-auto hover:bg-orange-500 focus:outline-none">
+            Buy Now
+          </a>
+      </div>
     </div>
+	
+	<section class="px-2 py-32 bg-white md:px-0">
+      <div class="mt-4 text-center">
+        <h3 class="text-2xl font-bold">Our Menu</h3>
+        <h2 class="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">
+          TODAY'S SPECIALITY</h2>
+      </div>
+      <div class="container w-full px-5 py-6 mx-auto">
+        <div class="grid lg:grid-cols-4 gap-y-6">
+          <div class="max-w-xs mx-4 mb-2 rounded-lg shadow-lg">
+            <img class="w-full h-48" src="https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032_960_720.jpg"
+              alt="Image" />
+            <div class="px-6 py-4">
+              <div class="flex mb-2">
+                <span class="px-4 py-0.5 text-sm bg-red-500 rounded-full text-red-50">Seafood</span>
+              </div>
+              <h4 class="mb-3 text-xl font-semibold tracking-tight text-green-600 uppercase">salmon fish 1 seafood</h4>
+              <p class="leading-normal text-gray-700">Lorem ipsum dolor, sit amet cons ectetur adipis icing
+                elit.</p>
+            </div>
+            <div class="flex items-center justify-between p-4">
+              <button class="px-4 py-2 bg-green-600 text-green-50">Order Now</button>
+              <span class="text-xl text-green-600">$20.0</span>
+            </div>
+          </div>
+          <div class="max-w-xs mx-4 mb-2 rounded-lg shadow-lg">
+            <img class="w-full h-48" src="https://cdn.pixabay.com/photo/2010/12/13/10/25/canape-2802_960_720.jpg"
+              alt="Image" />
+            <div class="px-6 py-4">
+              <div class="flex mb-2">
+                <span class="px-4 py-0.5 text-sm bg-pink-500 rounded-full text-pink-50">Seafood</span>
+              </div>
+              <h4 class="mb-3 text-xl font-semibold tracking-tight text-green-600 uppercase">salmon fish 2 seafood</h4>
+              <p class="leading-normal text-gray-700">Lorem ipsum dolor, sit amet cons ectetur adipis icing
+                elit.</p>
+            </div>
+            <div class="flex items-center justify-between p-4">
+              <button class="px-4 py-2 bg-green-600 text-green-50">Order Now</button>
+              <span class="text-xl text-green-600">$40.12</span>
+            </div>
+          </div>
+
+          <div class="max-w-xs mx-4 mb-2 rounded-lg shadow-lg">
+            <img class="w-full h-48" src="https://cdn.pixabay.com/photo/2015/04/08/13/13/food-712665_960_720.jpg"
+              alt="Image" />
+            <div class="px-6 py-4">
+              <div class="flex mb-2">
+                <span class="px-4 py-0.5 text-sm bg-red-500 rounded-full text-red-50">Seafood</span>
+              </div>
+              <h4 class="mb-3 text-xl font-semibold tracking-tight text-green-600 uppercase">salmon fish 3 seafood</h4>
+              <p class="leading-normal text-gray-700">Lorem ipsum dolor, sit amet cons ectetur adipis icing
+                elit.</p>
+            </div>
+            <div class="flex items-center justify-between p-4">
+              <button class="px-4 py-2 bg-green-600 text-green-50">Order Now</button>
+              <span class="text-xl text-green-600">$50.12</span>
+            </div>
+          </div>
+
+          <div class="max-w-xs mx-4 mb-2 rounded-lg shadow-lg">
+            <img class="w-full h-48" src="https://cdn.pixabay.com/photo/2015/10/02/15/59/olive-oil-968657_960_720.jpg"
+              alt="Image" />
+            <div class="px-6 py-4">
+              <div class="flex mb-2">
+                <span class="px-4 py-0.5 text-sm bg-pink-500 rounded-full text-pink-50">Tea</span>
+              </div>
+              <h4 class="mb-3 text-xl font-semibold tracking-tight text-green-600 uppercase">Fresh Tea</h4>
+              <p class="leading-normal text-gray-700">Lorem ipsum dolor, sit amet cons ectetur adipis icing
+                elit.</p>
+            </div>
+            <div class="flex items-center justify-between p-4">
+              <button class="px-4 py-2 bg-green-600 text-green-50">Order Now</button>
+              <span class="text-xl text-green-600">$4.00</span>
+            </div>
+          </div>
+          <div class="max-w-xs mx-4 mb-2 rounded-lg shadow-lg">
+            <img class="w-full h-48" src="https://cdn.pixabay.com/photo/2015/04/08/13/13/food-712665_960_720.jpg"
+              alt="Image" />
+            <div class="px-6 py-4">
+              <div class="flex mb-2">
+                <span class="px-4 py-0.5 text-sm bg-red-500 rounded-full text-red-50">Seafood</span>
+              </div>
+              <h4 class="mb-3 text-xl font-semibold tracking-tight text-green-600 uppercase">salmon fish 3 seafood</h4>
+              <p class="leading-normal text-gray-700">Lorem ipsum dolor, sit amet cons ectetur adipis icing
+                elit.</p>
+            </div>
+            <div class="flex items-center justify-between p-4">
+              <button class="px-4 py-2 bg-green-600 text-green-50">Order Now</button>
+              <span class="text-xl text-green-600">$50.12</span>
+            </div>
+          </div>
+
+          <div class="max-w-xs mx-4 mb-2 rounded-lg shadow-lg">
+            <img class="w-full h-48" src="https://cdn.pixabay.com/photo/2015/10/02/15/59/olive-oil-968657_960_720.jpg"
+              alt="Image" />
+            <div class="px-6 py-4">
+              <div class="flex mb-2">
+                <span class="px-4 py-0.5 text-sm bg-pink-500 rounded-full text-pink-50">Tea</span>
+              </div>
+              <h4 class="mb-3 text-xl font-semibold tracking-tight text-green-600 uppercase">Fresh Tea</h4>
+              <p class="leading-normal text-gray-700">Lorem ipsum dolor, sit amet cons ectetur adipis icing
+                elit.</p>
+            </div>
+            <div class="flex items-center justify-between p-4">
+              <button class="px-4 py-2 bg-green-600 text-green-50">Order Now</button>
+              <span class="text-xl text-green-600">$4.00</span>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
 </x-app-layout>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,117 +1,39 @@
-<nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
-    <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
-            <div class="flex">
-                <!-- Logo -->
-				@if (Route::has('login'))
-					<div class="shrink-0 flex items-center">
-                    @auth
-                        <a href="{{ route('beranda') }}">
-							<x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
-						</a>
-                    @else
-                        <a href="{{ route('dashboard') }}">
-							<x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
-						</a>
-                    @endauth
-					</div>
-				@endif
-
-                <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-					<x-nav-link :href="route('manajemen-produk')" :active="request()->routeIs('manajemen-produk')">
-                        {{ __('Manajemen Produk') }}
-                    </x-nav-link>
-					<x-nav-link :href="route('manajemen-pesanan')" :active="request()->routeIs('manajemen-pesanan')">
-                        {{ __('Manajemen Pesanan') }}
-                    </x-nav-link>
-					<x-nav-link :href="route('laporan-keuangan')" :active="request()->routeIs('laporan-keuangan')">
-                        {{ __('Laporan Keuangan') }}
-                    </x-nav-link>
-                </div>
-            </div>
-
-            <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ms-6">
-                <x-dropdown align="right" width="48">
-                    <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                            <div>{{ Auth::user()->name }}</div>
-
-                            <div class="ms-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                </svg>
-                            </div>
-                        </button>
-                    </x-slot>
-
-                    <x-slot name="content">
-                        <x-dropdown-link :href="route('profile.edit')">
-                            {{ __('Profile') }}
-                        </x-dropdown-link>
-
-                        <!-- Authentication -->
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-
-                            <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                {{ __('Log Out') }}
-                            </x-dropdown-link>
-                        </form>
-                    </x-slot>
-                </x-dropdown>
-            </div>
-
-            <!-- Hamburger -->
-            <div class="-me-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
-                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
-            </div>
-        </div>
-    </div>
-
-    <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
+<nav class="container px-6 py-8 mx-auto md:flex md:justify-between md:items-center">
+        <div class="flex items-center justify-between">
+          <a class="text-xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-orange-400 to-red-500 md:text-2xl hover:text-green-400"
+            href="#">
+            TailFood
+          </a>
+          <!-- Mobile menu button -->
+          <div @click="isOpen = !isOpen" class="flex md:hidden">
+            <button type="button" class="text-gray-800 hover:text-gray-400 focus:outline-none focus:text-gray-400"
+              aria-label="toggle menu">
+              <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
+                <path fill-rule="evenodd"
+                  d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z">
+                </path>
+              </svg>
+            </button>
+          </div>
         </div>
 
-        <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
-            <div class="px-4">
-                <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
-            </div>
-
-            <div class="mt-3 space-y-1">
-                <x-responsive-nav-link :href="route('profile.edit')">
-                    {{ __('Profile') }}
-                </x-responsive-nav-link>
-
-                <!-- Authentication -->
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-
-                    <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                        {{ __('Log Out') }}
-                    </x-responsive-nav-link>
-                </form>
-            </div>
+        <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
+        <div :class="isOpen ? 'flex' : 'hidden'"
+          class="flex-col mt-8 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0">
+          <a class="text-transparent bg-clip-text bg-gradient-to-r from-orange-400 to-red-500 hover:text-green-400"
+            href="/dashboard">Dashboard</a>
+          <a class="text-transparent bg-clip-text bg-gradient-to-r from-orange-400 to-red-500 hover:text-green-400"
+            href="/manajemen-produk">Manajemen Produk</a>
+          <a class="text-transparent bg-clip-text bg-gradient-to-r from-orange-400 to-red-500 hover:text-green-400"
+            href="/manajemen-pesanan">Manajemen Pesanan</a>
+          <a class="text-transparent bg-clip-text bg-gradient-to-r from-orange-400 to-red-500 hover:text-green-400"
+            href="/laporan-keuangan">Laporan Keuangan</a>
+          <div>
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-gray-400" fill="none" viewBox="0 0 24 24"
+              stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+            </svg>
+          </div>
         </div>
-    </div>
-</nav>
+      </nav>
